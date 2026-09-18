9aa551eda54457797e2ca0818753c5ff1308f032
Ritika <ritikahari2006@gmail.com>
2026-09-18 21:40:37 +0530

    update backend

--- Controller/email.js
+++ Controller/email.js
@@ -1,24 +1,32 @@
-const nodemailer =require("nodemailer");
-const transporter=nodemailer.createTransport({
-    service:"gmail",
-    auth:{
-        user:"ritikahari2006@gmail.com",
-        pass:"ulfk brfk abbk gknz"
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "ritikahari2006@gmail.com",
+        pass: "ulfk brfk abbk gknz"
     }
 });
-const sendEmail=async(to,subject,text)=>{
-    try{
-        const mailOptions={
-            from:"ritikahari2006@gmail.com",   
-            to:to,
-            subject:subject,
-            text:text
+
+const sendEmail = async (to, subject, text) => {
+    try {
+        const mailOptions = {
+            from: process.env.EMAIL_USER,
+            to: to,
+            subject: subject,
+            text: text
         };
-        const info=await transporter.sendMail(mailOptions);
-        console.log("Email sent  successfully:",info.response);
+
+        const info = await transporter.sendMail(mailOptions);
+
+        console.log("Email sent successfully:", info.response);
+
+        return true;
+
     } catch (error) {
-        console.error("Error sending email:", error);
-    }   
-    return false;
+        console.log("Email error:", error);
+        throw error;
+    }
 };
-module.exports={sendEmail};
+
+module.exports = { sendEmail };
